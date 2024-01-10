e26360a76c2b7308c4cac6a8296c70af33433e96
Chenna kesava reddy <89239479+chenna8464@users.noreply.github.com>
2024-01-10 16:54:30 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class MyApp extends StatelessWidget {
           ),
         ],
         child: MaterialApp(
-          debugShowCheckedModeBanner: true,
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
